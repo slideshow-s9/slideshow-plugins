913e69afb1b2baa6c3d0dc42c5473ceab1644c5e
Lucas Velasco <lucasv@ar.ibm.com>
2016-04-19 10:25:51 -0300

    adding center to Tables Helper

--- tables/tables_helper.rb
+++ tables/tables_helper.rb
@@ -1,23 +1,23 @@
 module Slideshow
   module TablesHelper
 
-# todo: add center, plus generic col helper
+# todo: add generic col helper
 
 
 def left( opts={}, &blk )
-   
+
   width = opts.fetch( :width, "50%" )
   clazz = opts.fetch( :class, nil )
-   
+
   puts "  Adding HTML for left column (using table layout)..."
-    
+
   text = capture_erb(&blk)
-      
+
   before  = "<!-- begin left #{opts.inspect} -->\n"
-  before << "<div class='#{clazz}'>\n"  if clazz  
+  before << "<div class='#{clazz}'>\n"  if clazz
   before << "<table width='100%'><tr><td width='#{width}' markdown='block' style='vertical-align: top;'>\n"
-    
-  after   = "</td>\n"  
+
+  after   = "</td>\n"
   after  << "<!-- end left -->\n"
 
   html  = ""
@@ -33,27 +33,51 @@ def right( opts={}, &blk )
 
   width = opts.fetch( :width, "50%" )
   clazz = opts.fetch( :class, nil )
-   
+
   puts "  Adding HTML for right column (using table layout)..."
-    
+
   text = capture_erb(&blk)
-    
-  before  = "<!-- begin right #{opts.inspect} -->\n" 
-  before << "<td width='#{width}' markdown='block' style='vertical-align: top;'>\n"  
-    
+
+  before  = "<!-- begin right #{opts.inspect} -->\n"
+  before << "<td width='#{width}' markdown='block' style='vertical-align: top;'>\n"
+
   after   = "</td></tr></table>\n"
-  after  << "</div>\n"  if clazz    
+  after  << "</div>\n"  if clazz
   after  << "<!-- end right -->\n"
 
   html  = ""
   html << guard_block( before )
   html << text
   html << guard_block( after )
-      
+
   concat_erb( html, blk.binding )
   return    ## todo/check: do we need a return?
 end
 
+def center( opts={}, &blk )
+
+  width = opts.fetch( :width, "100%" )
+  clazz = opts.fetch( :class, nil )
+
+  puts "  Adding HTML for center column (using table layout)..."
+
+  text = capture_erb(&blk)
+
+  before  = "<!-- begin center #{opts.inspect} -->\n"
+  before << "<div class='#{clazz}'>\n"  if clazz
+  before << "<table width='100%'><tr><td width='#{width}' markdown='block' style='vertical-align: top; text-align: center;'>\n"
+
+  after   = "</td></tr></table>\n"
+  after  << "<!-- end center -->\n"
+
+  html  = ""
+  html << guard_block( before )
+  html << text
+  html << guard_block( after )
+
+  concat_erb( html, blk.binding )
+  return    ## todo/check: do we need a return?
+end
 
 end # module TablesHelper
 end # module Slideshow
